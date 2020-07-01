63eee3bda0451bf5b9b08680028877e49918ca72
Stanly Lau <stanly@odd-e.com>
2020-07-01 16:29:09 +0800

    First commit

---
+++ julie/plaintext_to_html_converter.js
@@ -0,0 +1,64 @@
+const notifier = require("./notifier.js")
+const fs = require('fs')
+
+class PlaintextToHtmlConverter {
+    toHtml() {
+        const text = fs.readFileSync('./henry/sample.txt', 'utf8')
+        const htmlLines = this._basicHtmlEncode(text)
+        notifier.notify('HTML encoding done.')
+        return htmlLines
+    }
+
+    _basicHtmlEncode(source) {
+        var stashNextCharacterAndAdvanceThePointer = () => {
+            var c = source.charAt(i)
+            i += 1
+            return c
+
+        };
+
+        var addANewLine = () => {
+            convertedLine = convertedLine.join('')
+            result.push(convertedLine)
+            convertedLine = []
+        };
+
+        var pushACharacterToTheOutput = () => {
+            convertedLine.push(characterToConvert);
+        };
+
+        var i = 0;
+        var result = [];
+        var convertedLine = [];
+        var characterToConvert = stashNextCharacterAndAdvanceThePointer();
+        while (i <= source.length) {
+
+            switch (characterToConvert) {
+                case '<':
+                    convertedLine.push('&lt;');
+                    break;
+                case '>':
+                    convertedLine.push('&gt;');
+                    break;
+                case '&':
+                    convertedLine.push('&amp;');
+                    break;
+                case '\n':
+                    addANewLine();
+                    break;
+                default:
+                    pushACharacterToTheOutput();
+            }
+
+            characterToConvert = stashNextCharacterAndAdvanceThePointer();
+        }
+
+        addANewLine();
+        result = result.join('<br />');
+        return result;
+    }
+}
+
+module.exports = {
+    PlaintextToHtmlConverter
+}
