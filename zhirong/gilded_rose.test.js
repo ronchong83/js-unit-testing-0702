cb73a3967f34d0efd3c4ce96fc6c59dc7ba09fb0
Zhang Zhirong <ronchong83@gmail.com>
2020-07-02 18:52:52 +0800

    gilded rose test

--- zhirong/gilded_rose.test.js
+++ zhirong/gilded_rose.test.js
@@ -1,54 +1,77 @@
 const { Shop, Item } = require("./gilded_rose.js")
 
 describe("Gilded Rose", () => {
-  it("should reduce sellin and increase quality", () => {
-    const gildedRose = new Shop([new Item("Aged Brie", 2, 2)])
+  it("should reduce sellIn and increase quality for Aged Brie", () => {
+    const agedBrie = new Item("Aged Brie", 2, 2)
+    const gildedRose = new Shop([agedBrie])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).toBe("Aged Brie")
-    expect(items[0].sellIn).toEqual(1)
-    expect(items[0].quality).toEqual(3)
+    expect(items[0].name).toBe(agedBrie.name)
+    expect(items[0].sellIn).toEqual(agedBrie.sellIn - 1)
+    expect(items[0].quality).toEqual(agedBrie.quality + 1)
   });
-  it("should reduce sellin and increase quality twice as fast for Aged Brie", () => {
-    const gildedRose = new Shop([new Item("Aged Brie", 0, 2)])
+  it("should reduce sellIn and increase quality twice as fast for Aged Brie sellin <= 0", () => {
+    const agedBrie = new Item("Aged Brie", 0, 2)
+    const gildedRose = new Shop([agedBrie])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).toBe("Aged Brie")
-    expect(items[0].sellIn).toEqual(-1)
-    expect(items[0].quality).toEqual(4)
+    expect(items[0].name).toBe(agedBrie.name)
+    expect(items[0].sellIn).toEqual(agedBrie.sellIn - 1)
+    expect(items[0].quality).toEqual(agedBrie.quality + 2)
   });
-  it("should reduce sellin and quality", () => {
-    const gildedRose = new Shop([new Item("Rose", 2, 2)])
+  it("should reduce sellIn and quality for Others", () => {
+    const others = new Item("Others", 2, 2)
+    const gildedRose = new Shop([others])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).toBe("Rose")
-    expect(items[0].sellIn).toEqual(1)
-    expect(items[0].quality).toEqual(1)
+    expect(items[0].name).toBe(others.name)
+    expect(items[0].sellIn).toEqual(others.sellIn - 1)
+    expect(items[0].quality).toEqual(others.quality - 1)
   });
-  it("should reduce sellin and increase quality", () => {
-    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 2, 2)])
+  it("should reduce sellIn and increase quality by 3 if sellIn < 6 for Backstage", () => {
+    const backStage = new Item("Backstage passes to a TAFKAL80ETC concert", 2, 2)
+    const gildedRose = new Shop([backStage])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert")
-    expect(items[0].sellIn).toEqual(1)
-    expect(items[0].quality).toEqual(5)
+    expect(items[0].name).toBe(backStage.name)
+    expect(items[0].sellIn).toEqual(backStage.sellIn - 1)
+    expect(items[0].quality).toEqual(backStage.quality + 3)
   });
-  it("should not change in sellin and quality", () => {
-    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 2, 2)])
+  it("should reduce sellIn and increase quality by 2 if sellIn >= 6 for Backstage", () => {
+    const backStage = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 2)
+    const gildedRose = new Shop([backStage])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros")
-    expect(items[0].sellIn).toEqual(2)
-    expect(items[0].quality).toEqual(2)
+    expect(items[0].name).toBe(backStage.name)
+    expect(items[0].sellIn).toEqual(backStage.sellIn - 1)
+    expect(items[0].quality).toEqual(backStage.quality + 2)
   });
-  it("should reduce quality if less than 0 sellin for others", () => {
-    const gildedRose = new Shop([new Item("Rose", -1, 2)])
+  it("should reduce sellIn and increase quality by 1 if sellIn >= 11 for Backstage", () => {
+    const backStage = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 2)
+    const gildedRose = new Shop([backStage])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).toBe("Rose")
-    expect(items[0].sellIn).toEqual(-2)
-    expect(items[0].quality).toEqual(0)
+    expect(items[0].name).toBe(backStage.name)
+    expect(items[0].sellIn).toEqual(backStage.sellIn - 1)
+    expect(items[0].quality).toEqual(backStage.quality + 1)
   });
-  it("should reduce to 0 quality if less than 0 sellin for Backstage", () => {
-    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", -1, 2)])
+  it("should not change in sellIn and quality for Sulfuras", () => {
+    const sulfuras = new Item("Sulfuras, Hand of Ragnaros", 2, 2)
+    const gildedRose = new Shop([sulfuras])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert")
-    expect(items[0].sellIn).toEqual(-2)
-    expect(items[0].quality).toEqual(0)
+    expect(items[0].name).toBe(sulfuras.name)
+    expect(items[0].sellIn).toEqual(sulfuras.sellIn)
+    expect(items[0].quality).toEqual(sulfuras.quality)
+  });
+  it("should reduce quality twice as fast if less than 0 sellIn for others", () => {
+    const others = new Item("Others", -1, 2)
+    const gildedRose = new Shop([others])
+    const items = gildedRose.updateQuality()
+    expect(items[0].name).toBe(others.name)
+    expect(items[0].sellIn).toEqual(others.sellIn - 1)
+    expect(items[0].quality).toEqual(others.quality - 2)
+  });
+  it("should reduce to 0 quality if less than 0 sellIn for Backstage", () => {
+    const backStage = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 5)
+    const gildedRose = new Shop([backStage])
+    const items = gildedRose.updateQuality()
+    expect(items[0].name).toBe(backStage.name)
+    expect(items[0].sellIn).toEqual(backStage.sellIn - 1)
+    expect(items[0].quality).toEqual(backStage.quality - backStage.quality)
   });
   
 });
